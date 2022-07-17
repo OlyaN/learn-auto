e62577b7f6bc69f5d62386690707096654d54443
OlyaN <nemyrovska.olya@gmail.com>
2022-07-17 14:42:01 +0300

    commit from Idea

--- HelloJava.java
+++ HelloJava.java
@@ -1,5 +1,5 @@
 public class HelloJava {
     public static void main(String args[]){
-        System.out.println("Hello World from another branch second try");
+        System.out.println("Hello World after re-install Idea");
     }
 }
